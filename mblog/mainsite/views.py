850b0f11d182a348dc294dd98025caac407ea768
Jianfeng.huang <laiooo000@163.com>
2017-08-20 23:56:19 +0800

    updated blog

--- mblog/mainsite/views.py
+++ mblog/mainsite/views.py
@@ -5,14 +5,31 @@ from django.shortcuts import render
 
 from .models import Post
 
+from datetime import datetime
+
 from django.http import HttpResponse
+
+from django.template.loader import get_template
+
+from django.utils import timezone
 
 # Create your views here.
 
+'''
 def homepage(request):
     posts = Post.objects.all()
     post_lists = list()
     for count, post in enumerate(posts):
         post_lists.append("NO.{}:".format(str(count)) + str(post) + "<br>")
+        post_lists.append("<small>" + str(post.body.encode('utf-8')) + "</small><br><br>")
     return HttpResponse(post_lists)
+'''
 
+def homepage(request):
+    template = get_template('index.html')
+    posts = Post.objects.all()
+    now = timezone.now()
+    #now = datetime.now()
+    html = template.render(locals())
+    return HttpResponse(html)
+    
