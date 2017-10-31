9a8b761b749e43ea13375a30b847310847414675
Jianfeng.huang <laiooo000@163.com>
2017-10-31 22:43:01 +0800

    again

--- alpha/post/views.py
+++ alpha/post/views.py
@@ -103,3 +103,24 @@ def carlist(request, maker='0'):
     html = template.render(locals())
     
     return HttpResponse(html)
+
+
+def carprice(request, maker='0'):
+    car_maker = ['Ford','Honda','Mazda']
+    car_list = [[ {'model':'Fiesta','price':203500},
+        {'model':'Focus','price':605000},
+        {'model':'Mustang','price':900000} ],
+        [ {'model':'Fit','price':450000},
+        {'model':'City','price':150000},
+        {'model':'NSX','price':1200000} ],
+        [ {'model':'Mazda3','price':329999},
+        {'model':'Mazda5','price':603000},
+        {'model':'Mazda6','price':850000} ]
+    ]
+    maker = int(maker)
+    maker_name = car_maker[maker] 
+    cars = car_list[maker]
+    template = get_template('carprice.html')
+    html = template.render(locals())
+
+    return HttpResponse(html)
