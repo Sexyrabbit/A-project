c39c9104f705de34f22995fecb870efe107b9e0a
Jianfeng.huang <laiooo000@163.com>
2017-11-15 23:55:01 +0800

    added nuclear power

--- ch07www/mysite/views.py
+++ ch07www/mysite/views.py
@@ -5,8 +5,15 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from django.template.loader import get_template
+
+from mysite import models
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("This is a test")
+    products = models.Product.objects.all()
+    template = get_template('index.html')
+    html = template.render(locals())
+    return HttpResponse(html)
  
